40aeba7f8020dc28f64f507c2d6da8023367591c
krick <antipin.konstantin@googlemail.com>
2009-05-10 13:07:53 +0200

    enhance comment-dwim-line function: now handles blank lines correclty

--- kemacs-development.el
+++ kemacs-development.el
@@ -22,8 +22,9 @@
 ;; http://www.opensubscriber.com/message/emacs-devel@gnu.org/10971693.html
 (defun comment-dwim-line (&optional arg)
   "Replacement for the comment-dwim command.
-If no region is selected and current line is not blank, then comment current line.
-Replaces default behaviour of comment-dwim, when it inserts comment at the end of the line"
+If no region is selected and current line is not blank and we are not at the end of the line,
+then comment current line.
+Replaces default behaviour of comment-dwim, when it inserts comment at the end of the line."
   (interactive "*P")
   (comment-normalize-vars)
   (if (and (not (region-active-p)) (not (looking-at "[ \t]*$")))
