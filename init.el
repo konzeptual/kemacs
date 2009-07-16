1783cb0c5f8678ad2d98de4b532826ed2fcceabc
krick <antipin.konstantin@googlemail.com>
2009-07-16 21:53:17 +0900

    remove obsolete cusomization

--- init.el
+++ init.el
@@ -40,8 +40,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(ediff-window-setup-function (quote ediff-setup-windows-plain))
- '(nxhtml-skip-welcome t)
- '(php-mode-force-pear t))
+ )
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
