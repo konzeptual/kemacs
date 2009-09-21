e2f6fb0a970609d8170bbd1797af3105e455decb
krick <antipin.konstantin@googlemail.com>
2009-09-21 16:15:08 +0900

    delete hedear2.el update some submodules

--- init.el
+++ init.el
@@ -35,17 +35,23 @@
 
 ;; Global settings done via emacs-config
 (custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
+  ;; custom-set-variables was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ '(comint-completion-addsuffix t)
+ '(comint-completion-autolist t)
+ '(comint-input-ignoredups t)
+ '(comint-move-point-for-output t)
+ '(comint-scroll-show-maximum-output t)
+ '(comint-scroll-to-bottom-on-input t)
  '(ediff-window-setup-function (quote ediff-setup-windows-plain))
- )
+ '(org-agenda-files (quote ("~/org/ideas.org" "~/org/journal.org" "~/org/notes.org" "~/org/reading.org" "~/org/reference.org" "~/org/tech.org" "~/org/todo.org" "~/org/up.org"))))
 (custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
+  ;; custom-set-faces was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
  )
 
 
