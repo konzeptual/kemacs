038ca4975a10464d996b8bcd7c9308f939f30730
krick <antipin.konstantin@googlemail.com>
2009-12-17 17:46:40 +0800

    .prawn extension should be opened with ruby-mode

--- kemacs-ruby.el
+++ kemacs-ruby.el
@@ -25,6 +25,7 @@
 (add-to-list 'auto-mode-alist '("Rakefile$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("\\.gemspec$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("Gemfile$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("\\.prawn$" . ruby-mode))
 
 ;; We never want to edit Rubinius bytecode
 (add-to-list 'completion-ignored-extensions ".rbc")
