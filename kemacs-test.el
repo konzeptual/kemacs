a59a0d44cb033b87017b0375890fe6e3d6eee197
krick <antipin.konstantin@googlemail.com>
2010-01-18 16:11:17 +0300

    ;; Usually I need rinary in git repos

--- kemacs-test.el
+++ kemacs-test.el
@@ -10,11 +10,18 @@
 ;; (require 'yasnippets-rspec)
 
 
+;;; Shell mode
+(setq ansi-color-names-vector ; better contrast colors
+      ["black" "red4" "green4" "yellow4"
+       "blue3" "magenta4" "cyan4" "white"])
+(add-hook 'shell-mode-hook 'ansi-color-for-comint-mode-on)
+;; (add-hook 'shell-mode-hook '(lambda () (toggle-truncate-lines 1)))
 
-(setenv "PAGER" "/bin/cat")
+;; in shell-mode prompt is read only
+(setq comint-prompt-read-only t)
 
-
-
+;; Usually I need rinary in git repos
+(add-hook 'git-status-mode-hook 'rinari-minor-mode)
 
 (provide 'kemacs-test)
 ;;; kemacs-test.el ends here
